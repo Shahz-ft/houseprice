3cf2637eb4df7fd26e74e3e8fddaf0a05a44f888
shahana beevi <117985160+Shahz-ft@users.noreply.github.com>
2025-06-30 12:52:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -37,5 +38,7 @@ def predict():
     
     return render_template('index.html', prediction_text=f"Predicted Price: {output}")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
